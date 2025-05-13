d9979165023f86cc2b65565d8f68b41cabe9d7bd
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-05-13 15:49:02 +0200

    spoko loko euro spoko

---
+++ java/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int CABLE_SIZE = 20;
+const int PC_NUMBER = 3;
+
+const char EMPTY_SYMBOL = '-';
+
+char cable[CABLE_SIZE];
+char computers[CABLE_SIZE];
+
+void init_cable() {
+    for(int i=0;i<CABLE_SIZE;i++) {
+        cable[i] = EMPTY_SYMBOL;
+    }
+}
+
+int main() {
+    computers[1] = ""
+    return 0;
+}
