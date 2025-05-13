b056a46111bb0c44bc86a4033dd6836e7606c5ac
Blazej Pawluk <bpawluk05@gmail.com>
2025-05-13 16:06:27 +0200

    symulacja giga

--- java/main.cpp
+++ java/main.cpp
@@ -10,12 +10,19 @@ const int PC_NUMBER = 3;
 
 const char EMPTY_SYMBOL = '-';
 
-char cable[CABLE_SIZE];
+struct cablePart {
+    bool changed;
+    char symbol;
+};
+
+cablePart cable[CABLE_SIZE];
 char computers[CABLE_SIZE];
+
+const int TIME = 30;
 
 void initCable() {
     for(int i=0;i<CABLE_SIZE;i++) {
-        cable[i] = EMPTY_SYMBOL;
+        cable[i] = {false, EMPTY_SYMBOL};
         computers[i] = ' ';
     }
 }
@@ -24,7 +31,7 @@ void printCable() {
     cout << "\033[2J\033[1;1H";
     cout << "|";
     for(int i=0;i<CABLE_SIZE;i++) {
-        cout << cable[i];
+        cout << cable[i].symbol;
     }
     cout << "|";
     cout << endl << ' ';
@@ -45,8 +52,14 @@ int main() {
     computers[1] = 'A';
     computers[19] = 'B';
     printCable();
-    this_thread::sleep_for(chrono::seconds(1));
-    computers[17] = 'B';
-    printCable();
+
+    cable[1].symbol = 'A';
+    for (int second = 0; second < TIME; second++) {
+        this_thread::sleep_for(chrono::seconds(1));
+
+
+
+        printCable();
+    }
     return 0;
 }
