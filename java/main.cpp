6665fe0ea964cb3407a0a4e1b4f5f75e83026b92
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-05-13 16:26:16 +0200

    computer struct

--- java/main.cpp
+++ java/main.cpp
@@ -9,21 +9,33 @@ const int CABLE_SIZE = 20;
 const int PC_NUMBER = 3;
 
 const char EMPTY_SYMBOL = '-';
+const char CONFLICT_SYMBOl = '#';
 
 struct cablePart {
     bool changed;
     char symbol;
 };
 
+// dla komputerów ' ' id jest -1
+// dla reszty id po kolei od 0 do PC_NUMBER
+// zeby działała macierz natężeń
+struct computer {
+    int id;
+    char symbol;
+};
+
 cablePart cable[CABLE_SIZE];
-char computers[CABLE_SIZE];
+computer computers[CABLE_SIZE];
+
+// ile komunikatów chce wysłać komputer o danym id
+int matrix[PC_NUMBER] = {1, 2, 1};
 
 const int TIME = 30;
 
 void initCable() {
     for(int i=0;i<CABLE_SIZE;i++) {
         cable[i] = {false, EMPTY_SYMBOL};
-        computers[i] = ' ';
+        computers[i] = {-1, ' '};
     }
 }
 
@@ -36,21 +48,21 @@ void printCable() {
     cout << "|";
     cout << endl << ' ';
     for(int i=0;i<CABLE_SIZE;i++) {
-        if(computers[i] != ' ') {
+        if(computers[i].symbol != ' ') {
             cout << '|';
         } else cout << ' ';
     }
     cout << endl << ' ';
     for(int i=0;i<CABLE_SIZE;i++) {
-        cout << computers[i];
+        cout << computers[i].symbol;
     }
     cout << endl;
 }
 
 int main() {
     initCable();
-    computers[1] = 'A';
-    computers[19] = 'B';
+    computers[1] = {0, 'A'};
+    computers[19] = {1, 'B'};
     printCable();
 
     cable[1].symbol = 'A';
@@ -76,5 +88,6 @@ int main() {
 
         printCable();
     }
+
     return 0;
 }
