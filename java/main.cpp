11af411d44744b9b0c379669159c0cd18ed5a7ab
Dawid Dębkowski <dawid.debo2.pl@gmail.com>
2025-05-13 15:55:22 +0200

    inicjalizacja

--- java/main.cpp
+++ java/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -11,13 +13,40 @@ const char EMPTY_SYMBOL = '-';
 char cable[CABLE_SIZE];
 char computers[CABLE_SIZE];
 
-void init_cable() {
+void initCable() {
     for(int i=0;i<CABLE_SIZE;i++) {
         cable[i] = EMPTY_SYMBOL;
+        computers[i] = ' ';
     }
 }
 
+void printCable() {
+    cout << "\033[2J\033[1;1H";
+    cout << "|";
+    for(int i=0;i<CABLE_SIZE;i++) {
+        cout << cable[i];
+    }
+    cout << "|";
+    cout << endl << ' ';
+    for(int i=0;i<CABLE_SIZE;i++) {
+        if(computers[i] != ' ') {
+            cout << '|';
+        } else cout << ' ';
+    }
+    cout << endl << ' ';
+    for(int i=0;i<CABLE_SIZE;i++) {
+        cout << computers[i];
+    }
+    cout << endl;
+}
+
 int main() {
-    computers[1] = ""
+    initCable();
+    computers[1] = 'A';
+    computers[19] = 'B';
+    printCable();
+    this_thread::sleep_for(chrono::seconds(1));
+    computers[17] = 'B';
+    printCable();
     return 0;
 }
